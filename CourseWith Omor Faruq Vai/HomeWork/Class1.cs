f16799a5aabf68e6bb912ccbd095ff0c87e38c4b
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-08-15 17:52:09 +0600

    Initial commit

---
+++ CourseWith Omor Faruq Vai/HomeWork/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class Class1
+    {
+        Console.WriteLine("Class");
+    }
+}
